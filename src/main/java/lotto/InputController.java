699dc283673b860097c67ff995b68714b0a1daee
PUPAO <tkdldjstm0127@gmail.com>
2025-11-03 12:07:55 +0900

    feat(input) : 구입 금액 입력 코드 구현

---
+++ src/main/java/lotto/InputController.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputController {
+
+    public int inputMoney() {
+        int input = Integer.parseInt(Console.readLine());
+        return input;
+    }
+}
