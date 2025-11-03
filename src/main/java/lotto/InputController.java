170284c8bcd8716e2c5ab1263e632eb8c6b57212
PUPAO <tkdldjstm0127@gmail.com>
2025-11-03 12:19:34 +0900

    feat(input) : 당첨번호 입력 구현

--- src/main/java/lotto/InputController.java
+++ src/main/java/lotto/InputController.java
@@ -1,11 +1,20 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputController {
 
     public int inputMoney() {
         int input = Integer.parseInt(Console.readLine());
         return input;
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        return Arrays.stream(Console.readLine().split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
     }
 }
