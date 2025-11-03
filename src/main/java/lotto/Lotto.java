401f0f1fdaae5d0c8127bf4271990b29e97ced12
PUPAO <tkdldjstm0127@gmail.com>
2025-11-03 14:45:42 +0900

    feat(Lotto) : 당첨 번호와 일치 개수 확인 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,5 +16,7 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public int matchCount(List<Integer> winningNumbers) {
+        return (int) numbers.stream().filter(winningNumbers::contains).count();
+    }
 }
