ef0190ae95ebae682201fe6ad6ff7a0cc2891d30
PUPAO <tkdldjstm0127@gmail.com>
2025-11-03 15:30:21 +0900

    feat(Lotto) : 보너스 번호와 일치 여부 확인 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -22,12 +22,19 @@ class LottoTest {
     }
 
     @Test
-    void 복권에_맞는지_확인() {
+    void 복권에_맞는지_테스트() {
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
         int count = lotto.matchCount(List.of(6,2,7,4,5,1));
 
         assertThat(count).isEqualTo(5);
     }
 
+    @Test
+    void 보너스_숫자_포함_여부_테스트() {
+        Lotto lotto = new Lotto(List.of(7, 1, 6, 5, 3, 4));
+        boolean containsBonus = lotto.containsBonus(7);
+
+        assertThat(containsBonus).isEqualTo(true);
+    }
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
