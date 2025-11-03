99d435d23cf22a21fb494429746611faf53d5242
PUPAO <tkdldjstm0127@gmail.com>
2025-11-03 12:17:38 +0900

    feat(input) : 당첨번호 입력 테스트 구현

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.io.ByteArrayInputStream;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -25,6 +26,21 @@ class ApplicationTest extends NsTest {
 
         // then
         assertThat(result).isEqualTo(10000);
+        Console.close();
+    }
+
+    @Test
+    void 당첨_번호_입력_테스트() {
+        // given
+        System.setIn(new ByteArrayInputStream("1,2,3,4,5,6".getBytes()));
+        List<Integer> answer = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // when
+        InputController inputController = new InputController();
+        List<Integer> result = inputController.inputWinningNumbers();
+
+        // then
+        assertThat(result).isEqualTo(answer);
         Console.close();
     }
 
